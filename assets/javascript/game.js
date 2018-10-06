2191f1c3569541ff80acb0270c8429118eba0012
PhelanM <phelanmccall@yahoo.com>
2018-10-05 17:13:58 -0700

    added changing backgrounds and music and sound effects. Looks pretty much done.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,16 @@ var winsHTML = document.getElementById("wins");
 var limbsHTML = document.getElementById("limbs");
 var gameNameHTML = document.getElementById("gameName");
 var instructionsHTML = document.getElementById("instructions");
+var mainElement = document.getElementById("main");
+
+var zeldaMusic = document.getElementById("zeldaTheme");
+var megamanMusic = document.getElementById("megamanTheme");
+var metroidMusic = document.getElementById("metroidTheme");
+
+var levelUpMusic = document.getElementById("levelUp");
+var gameOverMusic = document.getElementById("gameOver");
+var letterRightMusic = document.getElementById("letterRight");
+var letterWrongMusic = document.getElementById("letterWrong");
 
 var game = {
   wins: 0,
@@ -10,14 +20,9 @@ var game = {
   gamesToGuess: ["zelda", "megaman", "metroid"],
   lettersCorrect: [],
   lettersUsed: ["#"],
-  backgrounds: {
-    zelda: "../images/zelda.jpg",
-    megaman: "../images/megaman.jpg",
-    metroid: "../images/metroid.jpg"
 
-  },
 
-  levelUp: function() {
+  levelUp: function () {
     this.wins++;
     this.limbs = 6;
     winsHTML.innerHTML = this.wins;
@@ -25,10 +30,27 @@ var game = {
     this.lettersCorrect = [];
     this.lettersUsed = ["#"];
     lettersUsedHTML.innerHTML = this.lettersUsed;
- 
+    levelUpMusic.load();
+    levelUpMusic.play();
+
+    switch (this.wins) {
+      case 1:
+        zeldaMusic.pause();
+        megamanMusic.load();
+        megamanMusic.loop = true;
+        megamanMusic.play();
+        break;
+      case 2:
+        megamanMusic.pause();
+        metroidMusic.load();
+        metroidMusic.loop = true;
+        metroidMusic.play();
+    }
+
   },
 
-  resetGAME: function() {
+  resetGAME: function () {
+
     this.wins = 0;
     this.limbs = 6;
     this.lettersUsed = ["#"];
@@ -37,10 +59,22 @@ var game = {
     lettersUsedHTML.innerHTML = this.lettersUsed;
     winsHTML.innerHTML = this.wins;
     limbsHTML.innerHTML = this.limbs;
+    mainElement.id = "zelda";
+
+    if (!megamanMusic.paused) {
+      megamanMusic.pause();
+    } else if (!metroidMusic.paused) {
+      metroidMusic.pause();
+    } else if (!zeldaMusic.pause) {
+      zeldaMusic.pause();
+    }
+    zeldaMusic.load();
+    zeldaMusic.play();
+
     this.updateGameName();
   },
 
-  updateGameName: function() {
+  updateGameName: function () {
     var name = "";
     for (var i = 0; i < this.gamesToGuess[this.wins].length; i++) {
       if (this.lettersCorrect.includes(this.gamesToGuess[this.wins][i])) {
@@ -52,48 +86,101 @@ var game = {
     gameNameHTML.innerHTML = name;
   },
 
-  updateGame: function(key) {
-    
-           if(this.wins === this.gamesToGuess.length){
-            alert("YOU WON THE GAME!");
-            if(confirm("Want to play again?")){
-               this.resetGAME();
-               }
-          }
+  updateGame: function (key) {
+    if (zeldaMusic.volume > 0.03) {
+      zeldaMusic.volume = 0.03;
+      megamanMusic.volume = 0.03;
+      metroidMusic.volume = 0.03;
+      gameOverMusic.volume = 0.03;
+      levelUpMusic.volume = 0.03;
+      letterRightMusic.volume = 0.03;
+      letterWrongMusic.volume = 0.03;
+    }
+
+    switch (this.wins) {
+      case 0:
+      if(zeldaMusic.currentTime < 5){
+        zeldaMusic.play();
+      }
+      break;
+      case 1:
+        mainElement.id = "megaman";
+        break;
+      case 2:
+        mainElement.id = "metroid";
+        break;
+      default:
+        break;
+
+    }
+
+
+    if (this.wins === this.gamesToGuess.length) {
+      alert("YOU WON THE GAME!");
+      if (confirm("Want to play again?")) {
+
+        this.resetGAME();
+      }
+    }
     this.updateGameName();
     instructionsHTML.innerHTML = "Press ENTER when word is done."
+
     if (key.length === 1 && key.charCodeAt(0) > 96 && key.charCodeAt(0) < 123) {
       if (
         this.gamesToGuess[this.wins].includes(key) &&
         !this.lettersCorrect.includes(key)
       ) {
         this.lettersCorrect.push(key);
+        letterRightMusic.load();
+        letterRightMusic.play();
         this.updateGameName();
-        if(gameNameHTML.innerHTML.replace(/ /g, "") == this.gamesToGuess[this.wins].toUpperCase()){
+        if (gameNameHTML.innerHTML.replace(/ /g, "") == this.gamesToGuess[this.wins].toUpperCase()) {
           this.levelUp();
-       }
-      
+        }
+
       } else if (
         !this.lettersUsed.includes(key) &&
         !this.lettersCorrect.includes(key)
       ) {
         this.lettersUsed.push(key);
+        letterWrongMusic.load();
+        letterWrongMusic.play();
         this.limbs--;
         limbsHTML.innerHTML = this.limbs;
         lettersUsedHTML.innerHTML = this.lettersUsed.toString().toUpperCase().replace(/,/g, " ");
         if (this.limbs == 0) {
-          alert("YOU LOST THE GAME :'(");
+          gameOverMusic.load();
+          gameOverMusic.play();
           this.resetGAME();
         }
       }
-      
-     
     }
-   
-  }
-} 
 
-document.onkeyup = function(event) {
+    if (key.toLowerCase() === " ") {
+      if (zeldaMusic.volume < 0.03) {
+      zeldaMusic.volume = 0.03;
+      megamanMusic.volume = 0.03;
+      metroidMusic.volume = 0.03;
+      gameOverMusic.volume = 0.03;
+      levelUpMusic.volume = 0.03;
+      letterRightMusic.volume = 0.03;
+      letterWrongMusic.volume = 0.03;
+      } else {
+        zeldaMusic.volume = 0;
+        megamanMusic.volume = 0;
+        metroidMusic.volume = 0;
+        gameOverMusic.volume = 0;
+        levelUpMusic.volume = 0;
+        letterRightMusic.volume = 0;
+        letterWrongMusic.volume = 0;
+      }
+    }
+  }
+
+
+}
+
+document.onkeyup = function (event) {
   var userKey = event.key.toLowerCase();
   game.updateGame(userKey);
 };
